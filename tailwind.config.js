27be2fb6b828e21ca92bbd6224104c9d8d21325e
Miraii133 <jiyovalmoria@gmail.com>
2023-01-11 22:12:39 +0800

    add: prelim activity#3.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./cards/index.html"
+
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
